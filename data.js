ff7db4254bc055b7103583f940e3f81b821c3533
Nargiza  Toboeva <117148489+Nargiza-24@users.noreply.github.com>
2023-08-21 11:59:20 +0600

    Update data.js

--- data.js
+++ data.js
@@ -10,11 +10,11 @@ export const arrays = {
           { "id": "mar", "type": "float", "caption": "Mar" }
         ],
         data: [
-          ["Mimesis", 1024.3, "commercial", true, 2048.2, {"d": 4096.0, "color": "selected"}, {"d": 7200.3, "color": "selected"}],
-          ["Mako", 2345.1, "internal", false, 3465.3, {"d": 12.5, "color": "selected"},{"d": 15.7, "color": "selected"}],
-          ["Edelweiss", 54.4, "commercial", false, 564.8, {"d": 4652.3, "color": "selected"},{"d": -3120.1, "color": "selected"}],
-          ["Forget-me-not", 930.1, "commercial", true, 120.7, {"d": 352.5, "color": "selected"},{"d": 4570.4, "color": "selected"}],
-          ["Aster", 930.1, "commercial", true, 120.7, {"d": 352.5, "color": "selected"},{"d": 4570.4, "color": "selected"}]
+          ["Mimesis", {"d": 1024.3, "color": "selected", "back": "special"}, "commercial", true, 2048.2, {"d": 4096.0, "color": "selected"}, {"d": 7200.3, "color": "selected"}],
+          ["Mako", {"d": 2345.1, "color": "selected", "back": "special"}, "internal", false, 3465.3, {"d": 12.5, "color": "selected"},{"d": 15.7, "color": "selected"}],
+          ["Edelweiss", {"d": 54.4, "color": "selected", "back": "special"}, "commercial", false, 564.8, {"d": 4652.3, "color": "selected"},{"d": -3120.1, "color": "selected"}],
+          ["Forget-me-not", {"d": 930.1, "color": "selected", "back": "special"}, "commercial", true, 120.7, {"d": 352.5, "color": "selected"},{"d": 4570.4, "color": "selected"}],
+          ["Aster", {"d": 930.1, "color": "selected", "back": "special"}, "commercial", true, 120.7, {"d": 352.5, "color": "selected"},{"d": 4570.4, "color": "selected"}]
         ]
       },
 
@@ -29,11 +29,11 @@ export const arrays = {
         { "id": "mar", "type": "float", "caption": "Mar" }
       ],
       data: [
-        ["Rose", 1111.1, "internal",true, 20.2, {"d": 409.0, "color": "selected"}, {"d": 720.3, "color": "selected"}],
-        ["Sunflower", 2345.6, "internal", true, 35.3, {"d": 124.5, "color": "selected"},{"d": 1544.7, "color": "selected"}],
-        ["Lavender", 547.4, "commercial", true, 5464.8, {"d": 652.3, "color": "selected"},{"d": 9.1, "color": "selected"}],
-        ["Peony", 9389.4, "commercial", false, 1270.7, {"d": 52.5, "color": "selected"},{"d": 40.4, "color": "selected"}],
-        ["Cosmos", 9378.1, "internal", true, 190.9, {"d": 3592.5, "color": "selected"},{"d": 4990.4, "color": "selected"}]
+        ["Rose", {"d": 1111.1, "color": "selected", "back": "special"}, "internal",true, 20.2, {"d": 409.0, "color": "selected"}, {"d": 720.3, "color": "selected"}],
+        ["Sunflower", {"d": 2345.6, "color": "selected", "back": "special"}, "internal", true, 35.3, {"d": 124.5, "color": "selected"},{"d": 1544.7, "color": "selected"}],
+        ["Lavender", {"d": 547.4, "color": "selected", "back": "special"}, "commercial", true, 5464.8, {"d": 652.3, "color": "selected"},{"d": 9.1, "color": "selected"}],
+        ["Peony", {"d": 9389.4, "color": "selected", "back": "special"}, "commercial", false, 1270.7, {"d": 52.5, "color": "selected"},{"d": 40.4, "color": "selected"}],
+        ["Cosmos", {"d": 9378.1, "color": "selected", "back": "special"}, "internal", true, 190.9, {"d": 3592.5, "color": "selected"},{"d": 4990.4, "color": "selected"}]
       ]
     },
     array3: {
@@ -49,7 +49,7 @@ export const arrays = {
           data: [
             ["Daisy", 1024.3, "commercial", false, 28.2, {"d": 6.0, "color": "selected"}, {"d": 9900.3, "color": "selected"}],
             ["Orchid", 2345.1, "internal", false, 65.3, {"d": 1772.5, "color": "selected"},{"d": -15.7, "color": "selected"}],
-            ["Tulip", 54.4, "commercial", false, 5764.8, {"d": 652.3, "color": "selected"},{"d": 720.1, "color": "selected"}],
+            ["Tulip", 540.4, "commercial", false, 5764.8, {"d": 652.3, "color": "selected"},{"d": 720.1, "color": "selected"}],
             ["Irises", 930.1, "internal", false, 1280.7, {"d": 2.5, "color": "selected"},{"d": -4570.4, "color": "selected"}],
             ["Poppy", 930.1, "commercial", true, 1209.7, {"d": 52.5, "color": "selected"},{"d": 770.4, "color": "selected"}]
           ]
